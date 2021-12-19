cfa14cd51d397f2b7d3209083c8a63697194bb0f
ShawnBroad <91346775+ShawnBroad@users.noreply.github.com>
2021-12-19 15:34:23 -0700

    started on main page
    Added some elements to the home page

--- src/comps/Navigation.js
+++ src/comps/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
     <>
     <Navbar fixed="top" bg="primary" variant="dark">
         <Container>
-            <Nav classname="me-auto">
+            <Nav className="me-auto">
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/">Home</Nav.Link>
             </Nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,58 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Navigation from "../comps/Navigation";
 
 export function Home() {
     return (
         <>
         <Navigation />
-        <br />
-        <br />
-        <br />
-        <h1>Home Page</h1>
+        <Container fluid className="headImage">
+            
+        </Container>
+        <Container className='intro'>
+            <Row>
+                <Col><h1>Hello</h1></Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Welcome to our page. This is a chat designed all around movies. We hope you join in on the conversation and enjoy your time here!</p>
+                </Col>
+            </Row>
+        </Container>
+            
+        <Container className='homeCC'>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>General</Card.Title>
+                        <Card.Text>General chat discussing your favorite/least favorite movies ect.</Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Cinematography</Card.Title>
+                        <Card.Text>All about Cinematography </Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Plot</Card.Title>
+                        <Card.Text>All about great movie plots</Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Directors</Card.Title>
+                        <Card.Text>A chat about all of the great directors out there</Card.Text>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+        
         </>
     );
 }
