0accc02954ba2fc36a322097c8c8ad6c15bf9e18
risonl <risonlain@gmail.com>
2021-12-22 13:51:28 -0700

    started on the chatroom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Navigation from "../comps/Navigation";
 import { Container, Col, Row,} from "react-bootstrap";
 import Marquee from "../comps/Marquee";
@@ -5,11 +7,61 @@ import Marquee from "../comps/Marquee";
 export function Home() {
     return (
         <>
+<<<<<<< HEAD
         <Navigation />  
         <br />
         <br />
         <br />
         <Marquee />
+=======
+        <Navigation />
+        <Container fluid className="headImage">
+            
+        </Container>
+        <Container className='intro'>
+            <Row>
+                <Col><h1>Hello</h1></Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Welcome to our page. This is a chat designed all around movies. We hope you join in on the conversation and enjoy your time here!</p>
+                </Col>
+            </Row>
+        </Container>
+            
+        <Container className='homeCC'>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>General</Card.Title>
+                        <Card.Text>General chat discussing your favorite/least favorite movies ect.</Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Cinematography</Card.Title>
+                        <Card.Text>All about Cinematography </Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Plot</Card.Title>
+                        <Card.Text>All about great movie plots</Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' href=""/>
+                        <Card.Title>Directors</Card.Title>
+                        <Card.Text>A chat about all of the great directors out there</Card.Text>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+>>>>>>> cfa14cd51d397f2b7d3209083c8a63697194bb0f
         
         </>
     );
