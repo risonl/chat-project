ec2b5199eb302541675f01cf8bb4d2742be8b545
ShawnBroad <91346775+ShawnBroad@users.noreply.github.com>
2022-01-14 14:34:34 -0700

    changed file structure
    moved files around

--- server/index.js
+++ server/index.js
@@ -9,26 +9,28 @@ import chatRoutes from './routes/chat.js';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded);
+app.use(express.urlencoded());
 app.use(cors())
 
 const PORT= 5000;
 const CONNECTION_URL = "mongodb+srv://arrayclass:array123@cluster0.wyyr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-app.post('/chat', (req, res) => {
-    const body = req.body;
+// app.post('/chat', (req, res) => {
+//     const body = req.body;
 
-    res.status(200).json([
-        body
-    ])
-}); 
+//     res.status(200).json([
+//         body
+//     ])
+// }); 
 
-app.get('/chat', (req,res) =>{
-    res.status(200).send("successful get")
-});
+// app.get('/chat', (req,res) =>{
+//     res.status(200).send("successful get")
+// });
 
 app.use('/chat', chatRoutes);
 
-mongoose.connect(CONNECTION_URL)
-    .then(()=> app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)))
-    .catch((err) => console.log(err.message));
+// mongoose.connect(CONNECTION_URL)
+//     .then(()=> app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)))
+//     .catch((err) => console.log(err.message));
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
