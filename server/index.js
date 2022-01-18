065d91ab456cb1d34cb8230560f7e8791bef3190
ShawnBroad <91346775+ShawnBroad@users.noreply.github.com>
2022-01-18 09:58:45 -0700

    socket added
    found the error and it is working. Now we just have to find out uses for it

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import  mongoose  from 'mongoose';
 import cors from 'cors';
-
 import chatRoutes from './routes/chat.js';
+import testRoutes from './routes/test.js';
+import  {Server}  from 'socket.io';
+import {createServer} from 'http';
 
 
 
@@ -10,7 +12,52 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors())
+app.use(cors());
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log("New Connection..." + socket.id);
+  socket.emit("greeting", "Welcome")
+  io.emit("greeting", "look who showed up, everyone!")
+  socket.broadcast.emit("greeting", "pretend you like them")
+
+  socket.on("userJoin", (room) => {
+    socket.join(room)
+  });
+
+  //send a message to everyone but the original sender
+  socket.on("message", (data) => {
+    //broadcast from the original sender socket
+    socket.broadcast.emit("serverMessage", data, socket.id);
+
+  });
+
+  socket.on("usermessage", (room, data) => {
+    socket.leave("public")
+    socket.join(room);
+    console.log(data);
+    socket.to(room).emit("serverMessage", data, socket.id); 
+    io.to(socket.id).emit("PM", `message sent: ${data}`) //this is how you PM 
+  });
+
+  
+
+  // Here is where we handle the disconnect of a socket.
+  socket.on("disconnect", (userList) => {
+    console.log("user disconnected");
+    //io.emit("userLeft", `${socket.id} disconnected`);
+  });
+
+  
+});
 
 const PORT= 5000;
 const CONNECTION_URL = "mongodb+srv://arrayclass:array123@cluster0.wyyr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -28,9 +75,10 @@ const CONNECTION_URL = "mongodb+srv://arrayclass:array123@cluster0.wyyr0.mongodb
 // });
 
 app.use('/chat', chatRoutes);
+app.use("/test", testRoutes)
 
 // mongoose.connect(CONNECTION_URL)
 //     .then(()=> app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)))
 //     .catch((err) => console.log(err.message));
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+httpServer.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
